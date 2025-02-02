0b1deba53d30080c6aea1e16389087e1f327827b
jocelyn.mutso <jocelyn.mutso@resys.io>
2025-02-02 12:10:33 +0200

    M1A-2 - M1A-6

--- src/exam-subjects/subject.M1A.ts
+++ src/exam-subjects/subject.M1A.ts
@@ -13,9 +13,75 @@ export const M1A: ExamApi.ErauSubject = {
     }, {
       correct: true, text: 'Vooluringi poolt vahelduvvoolule osutatavat takistust.',
     }, {
-      correct: false, text: 'Mahtuvust sisaldava vooluringi poolt vahelduvvoolule osutatavat takistust.',      
+      correct: false, text: 'Mahtuvust sisaldava vooluringi poolt vahelduvvoolule osutatavat takistust.',
     }, {
       correct: false, text: 'Juhtivust',
     }]
-  }]
+  }, {
+      id: 'M1A-2',
+      text: 'Kuidas nimetatakse mahtuvust ja induktiivsust sisaldava vooluringi poolt vahelduvvoole osutatavat takistust?',
+      info: '',
+      answers: [{
+        correct: false, text: 'Oom.'
+      }, {
+        correct: false, text: 'Dzhaul.'
+      }, {
+        correct: true, text: 'Impedants.'
+      }, {
+        correct: false, text: 'Juhtivuseks.'
+      }]
+    }, {
+      id: 'M1A-3',
+      text: 'Kuidas nimetatatkse induktiivsuse poolt vahelduvvoolule osutatavat takistust?',
+      info: '',
+      answers: [{
+        correct: false, text: 'Takistus.'
+      }, {
+        correct: true, text: 'Reaktiivtakistus.'
+      }, {
+        correct: false, text: 'Näivjuhtivus.'
+      }, {
+        correct: false, text: 'Impedants.'
+      }]
+    }, {
+      id: 'M1A-4',
+      text: 'Kuidas nimetatatkse mahtuvuse poolt vahelduvvoolule osutatavat takistust',
+      info: '',
+      answers: [{
+        correct: false, text: 'Takistus.'
+      }, {
+          correct: true, text: 'Reaktiivtakistus.'
+        }, {
+          correct: false, text: 'Näivjuhtivus.'
+        }, {
+          correct: false, text: 'Impedants.'
+        }]
+    }, {
+      id: 'M1A-5',
+      text: 'Kuidas reageerib induktiivsus vahelduvvoolule?',
+      info: '',
+      answers: [{
+        correct: false, text: 'Vahelduvvoolu sageduse suurenedes reaktiivtakustus väheneb.'
+      }, {
+        correct: false, text: 'Vahelduvvoolu amplituude suurenedes suureneb ka reaktiivtakistus.'
+      }, {
+        correct: true, text: 'Vahelduvvoolu sageduse suurenedes reaktiivsus suureneb.'
+      }, {
+        correct: false, text: 'Vahelduvvoolu amplituude suurenedes väheneb reaktiivtakistus.'
+      }]
+    }, {
+      id: 'M1A-5',
+      text: 'Kuidas reageerib mahtuvus vahelduvvoolule?',
+      info: '',
+      answers: [{
+        correct: true, text: 'Vahelduvvoolu sageduse suurenedes reaktiivtakustus väheneb.'
+      }, {
+        correct: false, text: 'Vahelduvvoolu amplituude suurenedes suureneb ka reaktiivtakistus.'
+      }, {
+        correct: false, text: 'Vahelduvvoolu sageduse suurenedes reaktiivsus suureneb.'
+      }, {
+        correct: false, text: 'Vahelduvvoolu amplituude suurenedes reaktiivtakistus väheneb.'
+      }]
+    },
+  ]
 }
